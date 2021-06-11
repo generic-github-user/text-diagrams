820b39bc4e6aefbe3c82264ddd7b979e3c9b6e67
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-11 12:38:21 -0400

    add Diagram class

--- diagrams.py
+++ diagrams.py
@@ -1,4 +1,13 @@
+import uuid
+
 shading = '░█'
 
 with open('./generated-diagram.txt', 'w') as file:
     file.write('test')
+
+class Diagram:
+    """A diagram containing some graphical elements and their relationships."""
+
+    def __init__(self, objects):
+        self.objects = objects if objects else []
+        self.id = uuid.uuid4()
