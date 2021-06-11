81d6b033eae68f2f91e008d0e28eddae68b81c6c
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-11 13:55:16 -0400

    add angled text rendering

--- diagrams.py
+++ diagrams.py
@@ -1,4 +1,5 @@
 import uuid
+import math
 
 shading = '░█'
 
@@ -13,14 +14,25 @@ class Element:
 class Text(Element):
     """Simple text element to add to a diagram"""
 
-    def __init__(self, pos, text):
+    def __init__(self, pos, text, angle=15):
         super(Text, self).__init__(pos)
         self.text = text
-        self.box = [len(self.text), 1]
+        self.l = len(self.text)
+        self.angle = angle
+        # self.ratio = math.sin(math.radians(self.angle))
+        r = math.radians(self.angle)
+        self.rx = math.cos(r)
+        self.ry = math.sin(r)
+        self.box = [round(self.l*self.rx+1), round(self.l*self.ry+1)]
         self.w, self.h = self.box
+        self.canvas = [[None for i in range(self.w)] for j in range(self.h)]
 
     def render(self):
-        return list(self.text)
+        for i, c in enumerate(self.text):
+            xc, yc = round(self.rx*i), round(self.ry*i)
+            print(xc, yc, self.box)
+            self.canvas[yc][xc] = c
+        return self.canvas
 
 
 class Diagram:
@@ -40,8 +52,10 @@ class Diagram:
             t = o.render()
             if t and type(t[0]) not in [list, tuple]:
                 t = [t]
-            for row in t:
-                self.canvas[o.y][o.x:o.x+o.w] = row
+            for i, row in enumerate(t):
+                for j, c in enumerate(row):
+                    if c:
+                        self.canvas[o.y+i][o.x+j] = c
 
         self.text = '\n'.join(''.join(row) for row in self.canvas)
 
