1b350ad58539a791dcf9f6853771d313032a79fc
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-11 12:43:04 -0400

    add additional diagram properties

--- diagrams.py
+++ diagrams.py
@@ -8,6 +8,10 @@ with open('./generated-diagram.txt', 'w') as file:
 class Diagram:
     """A diagram containing some graphical elements and their relationships."""
 
-    def __init__(self, objects):
+    def __init__(self, objects=None, dims=[30, 30], background='░'):
         self.objects = objects if objects else []
         self.id = uuid.uuid4()
+        self.canvas = []
+        self.dims = dims
+        self.x, self.y = self.dims
+        self.background = background
