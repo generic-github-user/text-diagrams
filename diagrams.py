316e66e20d136db78a7c5c2d9822c2fc559dba08
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-11 21:21:46 -0400

    expand docstring for Element class

--- diagrams.py
+++ diagrams.py
@@ -19,6 +19,15 @@ class Element:
 
     def __init__(self, pos):
         """Create a new element (note that this is an internal method used to create instances of element subclasses like the Text class)
+
+        Params:
+            pos: Coordinates of the element in the scene
+
+        Attributes:
+            pos: #
+            x: The x coordinate
+            y: The y coordinate
+            id: The UUID of this element
         """
 
         # Upper left-hand corner of bounding box (inclusive)
