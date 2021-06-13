1f24250ec70f4bb7fecb723acb181e079946b8aa
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-13 06:07:14 -0400

    add line module

---
+++ geometry/line.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+from point import Point
+
+class Line:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def move(self, delta):
+        for p in [self.a, self.b]:
+            p.move(delta)
+        return self
+
+    def rotate(self, *args, **kwargs):
+        for p in [self.a, self.b]:
+            p.rotate(*args, **kwargs)
+        return self
+
+    def divide(self, n=1):
+#         return [Line(Point(np.average([self.a, self.b], weights=[]))) for i in range(n)]
+        sections = []
+        for i in range(n):
+            a_ = np.average([self.a(), self.b()], weights=[i, n-i], axis=0)
+            b_ = np.average([self.a(), self.b()], weights=[i+1, n-i-1], axis=0)
+            s = Line(
+                Point(a_),
+                Point(b_)
+            )
+            sections.append(s)
+        return sections
+
+    def __str__(self):
+        return 'Line\n\t' + '\n\t'.join(str(v) for v in [self.a, self.b])
+
+l = Line(
+    Point([0, 2]),
+    Point([0, 1])
+)
+# l.rotate(t, 90).a.pos
+print(l.divide(3)[2])
