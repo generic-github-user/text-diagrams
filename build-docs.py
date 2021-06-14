67a291aa19329ac5dc99c1a77c7f0e7250f93c43
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-14 05:17:44 -0400

    more refactoring

--- build-docs.py
+++ build-docs.py
@@ -11,13 +11,104 @@ d_ = docs_directory
 output = d_+'/main.md'
 result = ''
 
+class Section:
+    def __init__(self, template_content):
+        self.helpers = Documentation()
+        self.template_content = template_content
+
+    def generate(self, stype, object, replacements):
+        docstring = object.__doc__
+        if docstring is None:
+            docstring = 'Not yet documented'
+        if stype in ['class', 'method']:
+            doc_info = self.helpers.extract_info(docstring)
+            replacements.append(('{docstring}', doc_info['text']['val'][0]))
+        else:
+            doc_info = object
+        content = self.template_content[stype]
+        for r in replacements:
+            content = content.replace(*r)
+
+        if stype == 'method':
+            param_list = []
+            if 'params' in doc_info:
+                for k, v in doc_info['params'].items():
+                    if k[0] in '!':
+                        k = k[1:] + ' [required]'
+                    if k[0] in '~':
+                        k = k[1:] + ' [optional]'
+
+                    param_content = generate_section('parameter', v, [('{parameter}', k)])
+                    param_list.append(param_content)
+            content = content.replace('[params]', '\n'.join(param_list))
+        elif stype == 'parameter':
+            # content = ''
+            label = doc_info[0]['label']
+
+            if '{ex' in label:
+                examples = []
+                ex_options = label[label.find('{ex'): label.find('}')+1]
+                # print(ex_options)
+                ex_options_ = [float(f) for f in ex_options.split(':')[1][:-1].split(',')]
+                ex_options_ = ex_options_ + [1., 1., 20.][-(3-len(ex_options_)):]
+                print(ex_options_)
+                for l in range(int(ex_options_[0])):
+                    bounds = ex_options_[1:3]
+                    # if ex_options_[1] % 1. == 0:
+                    if ex_options_[1].is_integer():
+                        ex = random.randint(*bounds)
+                    else:
+                        ex = random.uniform(*bounds)
+
+                    examples.append(str(ex))
+
+                examples = 'Examples: {}'.format('; '.join(['`{}`'.format(e) for e in examples]))
+                label = label.replace(ex_options, examples)
+                doc_info[0]['label'] = label
+            content = content.replace('{label}', label)
+
+            type_list = []
+            for t in doc_info:
+                print(t)
+                try:
+                    # print('[{}]'.format(','.join(t['type'])))
+                    y = [str(v) for v in t['type']]
+                    z = '[{}]'.format(','.join(y))
+                    print(z)
+                    z = z.replace("'", '"')
+                    # type_tags = set(json.loads(z))
+                    type_tags = json.loads(z)
+                    typestring = parse_tags(type_tags)
+                    print(True)
+                except Exception as e:
+                    print(e)
+                    typestring = ''
+                    for req in t['type']:
+                        if req in ['int', 'str', 'float', 'bool']:
+                            typestring += '`{}`'.format(req)
+                        elif req[0] == 'r':
+                            if ':' in req:
+                                limits = req[1:].split(':')
+                                typestring += ' between `{}` and `{}`'.format(*limits)
+                                # elif any(c in req for c in '<>')
+                                # elif '<' in req:
+                            elif any(s in req for s in symbols):
+                                typestring += ' {} {}'.format(symbols[req[1]], req[-1])
+
+                type_list.append('- {}: {}'.format(typestring, t['label']))
+
+            content = content.replace('[types]', '\n'.join(type_list))
+
+        return content
+
 
 class Documentation:
     """Documentation"""
 
-    def __init__(self, template_path='./docs/templates/*_template.md'):
+    def __init__(self, source_path='./*.py', template_path='./docs/templates/*_template.md'):
         template_files = glob.glob(template_path)
         # filename = t.split('/')[-1]
+        self.sources = {os.path.basename(s).split('.')[0]: os.path.normpath(s) for s in glob.glob(source_path)}
         self.templates = {os.path.basename(t).split('_')[0]: os.path.normpath(t) for t in template_files}
 
         self.template_content = {}
@@ -26,12 +117,14 @@ class Documentation:
             with open(path, 'r') as template_file:
                 self.template_content[k] = template_file.read()
 
+        self.text = ''
+        self.classes = []
 
-    def indent_width(s):
-        indent = len(s) - len(s.lstrip())
+    def indent_width(self, string):
+        indent = len(string) - len(string.lstrip())
         return indent
 
-    def clean_tabs(text):
+    def clean_tabs(self, text):
         lines = text.split('\n')
         if not lines[0]:
             lines = lines[1:]
@@ -59,36 +152,82 @@ class Documentation:
         output.append(block)
         return output
 
+    def import_modules(self):
+        for k, v in self.sources.items():
+            module_name = k
+            doc_module = importlib.import_module(module_name)
+            doc_classes = inspect.getmembers(doc_module, inspect.isclass)
+            # for c in doc_classes:
+            self.classes.append([doc_module, doc_classes])
+
+    def extract_info(self, docstring):
+        info = {}
+        docstring = self.clean_tabs(docstring)
+        lines = docstring.split('\n')
+        section = 'text'
+        subsection = ''
+        for l in lines:
+
+            t = self.indent_width(l)
+            if t == 0:
+                if l and l[0] == '@':
+                    section = l[1:]
+
+                if section not in info:
+                    info[section] = {}
+                if section == 'text':
+                    if 'val' not in info['text']:
+                        info['text']['val'] = []
+                    info[section]['val'].append(l)
+            elif t in [1, 4]:
+                subsection = self.clean_tabs(l)
+                if subsection not in info[section]:
+                    info[section][subsection] = []
+            elif t in [2, 8]:
+                parts = self.clean_tabs(l).split(': ')
+                label = parts[1]
+                type_info = parts[0][1:-1].replace(' ','').split(',')
+                arg_info = {
+                    'type': type_info,
+                    'label': label
+                }
+                info[section][subsection].append(arg_info)
+
+        return info
+
     def generate(self):
-        doc_module = importlib.import_module(module_name)
-        doc_classes = inspect.getmembers(doc_module, inspect.isclass)
-        for name, cls in doc_classes:
-            if cls.__module__ == module_name:
+        self.text = ''
+        self.import_modules()
 
-                # print(name, cls)
-                # print(docstring)
-                # try:
-                #     print(cls.__annotations__)
-                # except:
-                #     pass
+        for module, c in self.classes:
+            class_section = Section(self.template_content)
+            for name, cls in c:
+                # if cls.__module__ == module_name:
+                if True:
 
-                # section_content = section_content.replace('{class}', name)
-                # section_content = section_content.replace('{docstring}', docstring)
-                section_content = generate_section('class', cls, [('{class}', name)])
-                # print(section_content)
+                    # print(name, cls)
+                    # print(docstring)
+                    # try:
+                    #     print(cls.__annotations__)
+                    # except:
+                    #     pass
 
-                methods = inspect.getmembers(cls, predicate=inspect.isfunction)
-                # print(methods)
-                method_info = ''
-                for m in methods:
-                    subsection_content = generate_section('method', m[1], [('{method}', m[0])])
-                    method_info += subsection_content + '\n'
-                    # print(extract_info(mstring))
-                section_content = section_content.replace('[methods]', method_info)
-                section_content = section_content.replace('{timestamp}', str(datetime.datetime.now()))
+                    # section_content = section_content.replace('{class}', name)
+                    # section_content = section_content.replace('{docstring}', docstring)
+                    section_content = class_section.generate('class', cls, [('{class}', name)])
+                    # print(section_content)
 
+                    methods = inspect.getmembers(cls, predicate=inspect.isfunction)
+                    # print(methods)
+                    method_info = ''
+                    for m in methods:
+                        subsection_content = class_section.generate('method', m[1], [('{method}', m[0])])
+                        method_info += subsection_content + '\n'
+                        # print(extract_info(mstring))
+                    section_content = section_content.replace('[methods]', method_info)
+                    section_content = section_content.replace('{timestamp}', str(datetime.datetime.now()))
 
-                result += section_content + '\n'
+                    self.text += section_content + '\n'
 
     def write(self):
         # result = result.replace('{CA}', 'cellular automata')
@@ -108,43 +247,10 @@ class Documentation:
             file.write(result)
 
 Docs = Documentation()
-print(Docs.templates)
-
-def extract_info(s):
-    info = {}
-    s = clean_tabs(s)
-    lines = s.split('\n')
-    section = 'text'
-    subsection = ''
-    for l in lines:
-
-        t = indent_width(l)
-        if t == 0:
-            if l and l[0] == '@':
-                section = l[1:]
-
-            if section not in info:
-                info[section] = {}
-            if section == 'text':
-                if 'val' not in info['text']:
-                    info['text']['val'] = []
-                info[section]['val'].append(l)
-        elif t in [1, 4]:
-            subsection = clean_tabs(l)
-            if subsection not in info[section]:
-                info[section][subsection] = []
-        elif t in [2, 8]:
-            parts = clean_tabs(l).split(': ')
-            label = parts[1]
-            type_info = parts[0][1:-1].replace(' ','').split(',')
-            arg_info = {
-                'type': type_info,
-                'label': label
-            }
-            info[section][subsection].append(arg_info)
+print(Docs.templates, Docs.sources)
+Docs.generate()
 
 
-    return info
 
 symbols = {
     '<': 'less than',
@@ -157,90 +263,7 @@ symbols = {
 data_types = ['int', 'str', 'float', 'bool', 'func', 'array']
 
 
-def generate_section(stype, object, replacements):
-    docstring = object.__doc__
-    if docstring is None:
-        docstring = 'Not yet documented'
-    if stype in ['class', 'method']:
-        doc_info = extract_info(docstring)
-        replacements.append(('{docstring}', doc_info['text']['val'][0]))
-    else:
-        doc_info = object
-    content = template_content[stype]
-    for r in replacements:
-        content = content.replace(*r)
 
-    if stype == 'method':
-        param_list = []
-        if 'params' in doc_info:
-            for k, v in doc_info['params'].items():
-                if k[0] in '!':
-                    k = k[1:] + ' [required]'
-                if k[0] in '~':
-                    k = k[1:] + ' [optional]'
-
-                param_content = generate_section('parameter', v, [('{parameter}', k)])
-                param_list.append(param_content)
-        content = content.replace('[params]', '\n'.join(param_list))
-    elif stype == 'parameter':
-        # content = ''
-        label = doc_info[0]['label']
-
-        if '{ex' in label:
-            examples = []
-            ex_options = label[label.find('{ex'): label.find('}')+1]
-            # print(ex_options)
-            ex_options_ = [float(f) for f in ex_options.split(':')[1][:-1].split(',')]
-            ex_options_ = ex_options_ + [1., 1., 20.][-(3-len(ex_options_)):]
-            print(ex_options_)
-            for l in range(int(ex_options_[0])):
-                bounds = ex_options_[1:3]
-                # if ex_options_[1] % 1. == 0:
-                if ex_options_[1].is_integer():
-                    ex = random.randint(*bounds)
-                else:
-                    ex = random.uniform(*bounds)
-
-                examples.append(str(ex))
-
-            examples = 'Examples: {}'.format('; '.join(['`{}`'.format(e) for e in examples]))
-            label = label.replace(ex_options, examples)
-            doc_info[0]['label'] = label
-        content = content.replace('{label}', label)
-
-        type_list = []
-        for t in doc_info:
-            print(t)
-            try:
-                # print('[{}]'.format(','.join(t['type'])))
-                y = [str(v) for v in t['type']]
-                z = '[{}]'.format(','.join(y))
-                print(z)
-                z = z.replace("'", '"')
-                # type_tags = set(json.loads(z))
-                type_tags = json.loads(z)
-                typestring = parse_tags(type_tags)
-                print(True)
-            except Exception as e:
-                print(e)
-                typestring = ''
-                for req in t['type']:
-                    if req in ['int', 'str', 'float', 'bool']:
-                        typestring += '`{}`'.format(req)
-                    elif req[0] == 'r':
-                        if ':' in req:
-                            limits = req[1:].split(':')
-                            typestring += ' between `{}` and `{}`'.format(*limits)
-                            # elif any(c in req for c in '<>')
-                            # elif '<' in req:
-                        elif any(s in req for s in symbols):
-                            typestring += ' {} {}'.format(symbols[req[1]], req[-1])
-
-            type_list.append('- {}: {}'.format(typestring, t['label']))
-
-        content = content.replace('[types]', '\n'.join(type_list))
-
-    return content
 
 
 
