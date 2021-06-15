415a7d21b492143f25f06fc27a95dbaacc62982d
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-14 21:06:04 -0400

    add ArgRange class

--- argtype.py
+++ argtype.py
@@ -3,6 +3,16 @@ class ArgType:
     Defines a 'type' that can be used for validating and/or converting function arguments, generating examples, type hinting, etc. It is intended to emulate some of the features of Python's `typing` module, albeit with a more general scope.
     """
 
-    def __init__(self, primitive, **conditions):
+    def __init__(self, primitive, *conditions):
         self.primitive = primitive
         self.conditions = conditions
+
+class ArgRange:
+    """ArgRange"""
+
+    def __init__(self, *args):
+        # super(ArgRange, self).__init__()
+        self.range = range(*args)
+
+
+test = ArgType(int, ArgRange(1, 10, 2))
