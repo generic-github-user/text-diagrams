d88f606e2d5481b3061857f67af3ad5191311b61
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-14 20:58:50 -0400

    update parameters

--- argtype.py
+++ argtype.py
@@ -1,7 +1,8 @@
 class ArgType:
     """
-    Defines a 'type' that can be used for validating and/or converting function arguments, generating examples, etc. It is intended to emulate some of the features of Python's `typing` module, albeit with a more general scope.
+    Defines a 'type' that can be used for validating and/or converting function arguments, generating examples, type hinting, etc. It is intended to emulate some of the features of Python's `typing` module, albeit with a more general scope.
     """
 
-    def __init__(self, arg):
-        self.arg = arg
+    def __init__(self, primitive, **conditions):
+        self.primitive = primitive
+        self.conditions = conditions
