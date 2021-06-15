36468addfd47d1fdd0db5550f77f7ae037dc4690
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-14 21:16:29 -0400

    generate natural language summary of type

--- argtype.py
+++ argtype.py
@@ -6,6 +6,15 @@ class ArgType:
     def __init__(self, primitive, *conditions):
         self.primitive = primitive
         self.conditions = conditions
+        self.info = '{} {} {}'
+
+    def an(self, a):
+        return 'an' if a[0] in 'aeiou' else 'a'
+
+    def text(self):
+        condition_string = ', '.join(c.text() for c in self.conditions)
+        p = self.primitive.__name__
+        return self.info.format(self.an(p), p, condition_string)
 
 class ArgRange:
     """ArgRange"""
@@ -13,6 +22,11 @@ class ArgRange:
     def __init__(self, *args):
         # super(ArgRange, self).__init__()
         self.range = range(*args)
+        self.info = 'between {} and {}'
+        self.args = args
 
+    def text(self):
+        return self.info.format(*self.args)
 
-test = ArgType(int, ArgRange(1, 10, 2))
+test = ArgType(int, ArgRange(1, 10))
+print(test.text())
