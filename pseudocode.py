b91f1558b8d20ac2460e67133219459606f95c95
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-16 08:14:27 -0400

    move ops out of main file

--- pseudocode.py
+++ pseudocode.py
@@ -1,39 +1,11 @@
 import ast
 
+from python_ops import op_strings, symbols
+
 # class node_strings:
 #     For:
 
-op_strings = {
-    'Add': 'Increment',
-    'Sub': 'Decrement',
-    'In': 'contains',
-    'NotIn': 'does not contain',
-    'Eq': 'is equal',
-    'NotEq': 'is equal',
-    'Lt': 'is less than',
-    'LtE': 'is less than or equal to',
-    'Gt': 'is greater than',
-    'GtE': 'is greater than or equal to',
-}
 reversed = ['In', 'NotIn']
-
-symbols = {
-    'Add': '+',
-    'Mult': '*',
-    'FloorDiv': '//',
-    'Div': '/',
-    'Pow': '^',
-    'Sub': '-',
-    'Mod': '%',
-    'Eq': '=',
-    'NotEq': '!=',
-    'Lt': '<',
-    'LtE': '<=',
-    'Gt': '>',
-    'GtE': '>=',
-    'LShift': '<<',
-    'RShift': '>>',
-}
 
 functions = {
     'range': ('the range {} to {}', (0, None)),
---
+++ python_ops.py
@@ -0,0 +1,30 @@
+op_strings = {
+    'Add': 'Increment',
+    'Sub': 'Decrement',
+    'In': 'contains',
+    'NotIn': 'does not contain',
+    'Eq': 'is equal',
+    'NotEq': 'is equal',
+    'Lt': 'is less than',
+    'LtE': 'is less than or equal to',
+    'Gt': 'is greater than',
+    'GtE': 'is greater than or equal to',
+}
+
+symbols = {
+    'Add': '+',
+    'Mult': '*',
+    'FloorDiv': '//',
+    'Div': '/',
+    'Pow': '^',
+    'Sub': '-',
+    'Mod': '%',
+    'Eq': '=',
+    'NotEq': '!=',
+    'Lt': '<',
+    'LtE': '<=',
+    'Gt': '>',
+    'GtE': '>=',
+    'LShift': '<<',
+    'RShift': '>>',
+}
