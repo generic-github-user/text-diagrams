947dcbe50de5892b4fbd01b986e2830dd3849985
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-16 06:25:49 -0400

    add template strings for different node types

---
+++ pseudocode.py
@@ -0,0 +1,11 @@
+import ast
+
+# class node_strings:
+#     For:
+
+node_strings = {
+    'For': ('For {} in {}:', 'target', 'iter'),
+    'Name': ('{}', 'id'),
+    'Assign': ('Set {} to {}', 'targets', 'value'),
+    'Constant': ('{}', 'value')
+}
